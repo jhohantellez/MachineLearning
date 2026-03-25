f74f3e0a92ccc533111512fcbf0ba47f86fb78be
jhohantellez <jhohan.tellez.p@gmail.com>
2026-03-25 09:00:25 -0500

    Add linear regression module and fix conflicts

--- app.py
+++ app.py
@@ -1,15 +1,34 @@
-from flask import Flask, render_template
+
+from flask import Flask, render_template, request
+import joblib
 
 app= Flask(__name__)
+model = joblib.load("model.pkl")
 
-#create new route
-@app.route('/')
-
-#defined function
-def home ():
-    return 'hello flask'
-
-@app.route("/FirstPage")
-def firstPage():
+@app.route("/")
+def Home():
     return render_template('index.html')
+
+@app.route("/linearregression/concepts")
+def concepts():
+    return render_template("linear_regression/basic_concepts.html")
+
+@app.route("/linearregression/application", methods=["GET", "POST"])
+def linear_regression():
+    prediction = None
+
+    if request.method == "POST":
+        experience = float(request.form["experience"])
+        skills = float(request.form["skills"])
+        certifications = float(request.form["certifications"])
+
+
+        prediction = model.predict([[experience, skills, certifications]])[0]
+
+    return render_template("linear_regression/application.html", prediction=prediction)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
 print(app.url_map)
