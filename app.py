683cdf90dafa1ffabccf0a948f475d8d6a7d341f
jhohantellez <jhohan.tellez.p@gmail.com>
2026-03-25 17:12:38 -0500

    The app.py file was updated and home.html was added.

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ model = joblib.load("model.pkl")
 
 @app.route("/")
 def Home():
+    return render_template('home.html')
+
+@app.route("/linearregresion")
+def linearregression():
     return render_template('index.html')
 
 @app.route("/linearregression/concepts")
@@ -14,7 +18,7 @@ def concepts():
     return render_template("linear_regression/basic_concepts.html")
 
 @app.route("/linearregression/application", methods=["GET", "POST"])
-def linear_regression():
+def application():
     prediction = None
 
     if request.method == "POST":
