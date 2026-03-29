51def52c45f9ed0eefaa2e326f382e37c3f04064
Andrey Lopez <andreyestebanlopezguayazan@gmail.com>
2026-03-29 17:48:38 -0500

    Add routes for MultinomialNB and design of the concept section

--- app.py
+++ app.py
@@ -1,35 +1,38 @@
 from flask import Flask, render_template, request
 import joblib
 
-
-app= Flask(__name__)
+app = Flask(__name__)
 model = joblib.load("model.pkl")
 
+#-------------------- HOME --------------------
 @app.route("/")
-def Home():
+def home():
     return render_template('home.html')
 
+
+#-------------------- USE CASES --------------------
 @app.route("/usecases")
-def UseCase():
+def usecases():
     return render_template('usecases.html')
 
-#-----------------Use Cases-------------------------------------------
 @app.route("/UseCase/1")
-def UseCase1():
+def usecase1():
     return render_template('use_cases/use_case1.html')
 
 @app.route("/UseCase/2")
-def UseCase2():
+def usecase2():
     return render_template("use_cases/use_case2.html")
 
 @app.route("/UseCase/3")
-def UseCase3():
+def usecase3():
     return render_template("use_cases/use_case3.html")
 
 @app.route("/UseCase/4")
-def UseCase4():
+def usecase4():
     return render_template("use_cases/use_case4.html")
 
+
+#-------------------- LINEAR REGRESSION --------------------
 @app.route("/linearregression")
 def linearregression():
     return render_template('linearregression.html')
@@ -47,15 +50,30 @@ def application():
         skills = float(request.form["skills"])
         certifications = float(request.form["certifications"])
 
-
         prediction = model.predict([[experience, skills, certifications]])[0]
 
     return render_template("linear_regression/application.html", prediction=prediction)
 
 
-#-------------------------------------------------------------------------
+#-------------------- MULTINOMIAL NAIVE BAYES --------------------
+#-------------------- MULTINOMIAL MENU --------------------
+@app.route("/multinomial")
+def multinomial_menu():
+    return render_template("multinomial_nb.html")
+
+
+#-------------------- MULTINOMIAL CONCEPTS --------------------
+@app.route("/multinomial/concepts")
+def multinomial_concepts():
+    return render_template("MultinomialNB/Basic_conceptsnb.html")
+
+
+#-------------------- MULTINOMIAL APPLICATION --------------------
+@app.route("/multinomial/application")
+def multinomial_application():
+    return render_template("MultinomialNB/Applicationnb.html")
+#-------------------- RUN --------------------
 if __name__ == "__main__":
     app.run(debug=True)
-
 
 print(app.url_map)
